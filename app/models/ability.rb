f403785b28fdd3e06c304de643af074394b90f94
C-yoshida618 <kanamakihira@gmail.com>
2024-07-24 15:38:22 +0900

    Abilityクラスの設定

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,14 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+      can :manage, Product, user_id: user.id
+    end
     # Define abilities for the user here. For example:
     #
     #   return unless user.present?
