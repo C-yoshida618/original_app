789995646aea788a47c99ce85f953a079e07be28
C-yoshida618 <kanamakihira@gmail.com>
2024-07-25 07:24:09 +0900

    Revert "7月25日ファイル追加"
    This reverts commit 7529e530b68ca33b3721865e3547d2bc331bf2c2.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,3 @@
 class OrdersController < ApplicationController
-  def new
-    @order = Order.new
-  end
-
-  def create
-    @order = Order.new(order_params)
-    if @order.save
-      redirect_to complete_orders_path
-    else
-      render :new
-    end
-  end
-
-  def complete
-  end
-
-  private
-
-  def order_params
-    params.require(:order).permit(:user_id, :product_id, :shipping_cost)
-  end
+  load_and_authorize_resource
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'main#index'
-
-  get 'portfolio', to: 'portfolio#index'
-  get 'onlineshop', to: 'onlineshop#index'
-
   devise_for :users
-
-  resources :orders, only: [:new, :create] do
-    collection do
-      get 'complete'
-    end
-  end
-
-  namespace :admin do
-    resources :products, only: [:new, :create, :edit, :update, :index, :destroy]
-  end
+  resources :products
+  resources :orders
+  resources :addresses
+  root to: "products#index"
 end
