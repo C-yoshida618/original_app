a33af31836428eabfbf34df49322f219f6a5c7aa
C-yoshida618 <kanamakihira@gmail.com>
2024-07-26 22:34:49 +0900

    0726変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,19 @@ Rails.application.routes.draw do
   
   root to: 'main#index'
 
-  get 'main', to: 'main#index' #TOPページ
-  get 'main', to: 'main#new' #ポートフォリオページ
-  get 'products', to: 'products#index' #オンラインページ
-  get 'products', to: 'products#new' #出品ページ
-  get 'orders', to: 'orders#index' #オンラインページ
-  get 'orders', to: 'orders#new' #出品ページ
-  
+  get 'main', to: 'main#index' # TOPページ
+  get 'main/show', to: 'main#show', as: 'portfolio' # ポートフォリオページ
+
+  get 'products', to: 'products#index' # オンラインページ
+  get 'products/new', to: 'products#new' # 出品ページ
+
   resources :products do 
     resources :orders, only: [:new, :create]
-end
+  end
+
   resources :users do
     resources :orders, only: [:index]
-end
+  end
 
   resources :orders, only: [:show, :edit, :update, :destroy]
-end  
+end
