9e0b106b47aba372c2a37b395136ffae7867448a
C-yoshida618 <kanamakihira@gmail.com>
2024-07-26 15:51:52 +0900

    変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,26 +1,22 @@
 class OrdersController < ApplicationController
-  # 認証や認可のフィルターを追加できます
-  before_action :authenticate_user!, only: [:new, :creates]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
     # 注文の一覧を取得する
-    @orders = current_user.orders.includes(:product)
-    @products = @orders.map(&:product)
+    @orders = Order.all
+    @product = Product.all
   end
 
   def show
-    # 特定の注文を表示する
     @order = Order.find(params[:id])
     @product = @order.product
   end
 
   def new
-    # 新しい注文を作成するためのフォームを表示する
     @order = Order.new
   end
 
   def create
-    # 新しい注文を作成する
     @order = Order.new(order_params)
     if @order.save
       redirect_to @order, notice: 'Order was successfully created.'
@@ -30,12 +26,10 @@ class OrdersController < ApplicationController
   end
 
   def edit
-    # 注文を編集するためのフォームを表示する
     @order = Order.find(params[:id])
   end
 
   def update
-    # 注文を更新する
     @order = Order.find(params[:id])
     if @order.update(order_params)
       redirect_to @order, notice: 'Order was successfully updated.'
@@ -45,7 +39,6 @@ class OrdersController < ApplicationController
   end
 
   def destroy
-    # 注文を削除する
     @order = Order.find(params[:id])
     @order.destroy
     redirect_to orders_url, notice: 'Order was successfully destroyed.'
@@ -53,8 +46,7 @@ class OrdersController < ApplicationController
 
   private
 
-  # Strong Parameters
   def order_params
-    params.require(:order).permit(:product_id, :shipping_cost)
+    params.require(:order).permit(:user_id, :product_id, :shipping_cost)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,19 @@ Rails.application.routes.draw do
   
   root to: 'main#index'
 
-  get 'main', to: 'main#new'
+  get 'main', to: 'main#index' #TOPページ
+  get 'main', to: 'main#new' #ポートフォリオページ
+  get 'products', to: 'products#index' #オンラインページ
+  get 'products', to: 'products#new' #出品ページ
+  get 'orders', to: 'orders#index' #オンラインページ
+  get 'orders', to: 'orders#new' #出品ページ
   
   resources :products do 
     resources :orders, only: [:new, :create]
 end
-  resources :orders, only: [:index, :show, :edit, :update, :destroy]
-
+  resources :users do
+    resources :orders, only: [:index]
 end
+
+  resources :orders, only: [:show, :edit, :update, :destroy]
+end  
