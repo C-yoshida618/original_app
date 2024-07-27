d6c124f15cdcaa297644b0eaaab5da6b2320211c
C-yoshida618 <kanamakihira@gmail.com>
2024-07-27 10:33:07 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,9 +9,14 @@ class Product < ApplicationRecord
   with_options presence: true do
     validates :image
     validates :product_name, length: { maximum: 40 }
-    validates :description, length: { maximum: 1000 }
+    validates :description, length: { maximum: 1000 }, unless: :was_attached?
+
 
     # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
+    def was_attached?
+      self.image.attached?
+    end
   end
 end
